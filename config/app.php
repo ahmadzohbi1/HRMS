e16048e4baff9801ac7d2e422ebe2adc4785fb0e
ahmadzohbi1 <85623046+ahmadzohbi1@users.noreply.github.com>
2025-05-06 14:46:16 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'ahmad'
+    
     'name' => env('APP_NAME', 'Alzohbi'),
     'company_name' => env('COMPANY_NAME', 'Alzohbi'),
 
