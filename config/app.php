24431835d6d646481f86402bbc08a51c399999f7
ahmadzohbi1 <ahmadzohby1999@gmail.com>
2025-10-14 12:44:06 +0300

    c

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://office.utopialebanon.org'),
 
-    'asset_url' => env('ASSET_URL', 'http://localhost:8888/hrms/public'),
+    'asset_url' => env('ASSET_URL', 'https://office.utopialebanon.org/public'),
 
     /*
     |--------------------------------------------------------------------------
